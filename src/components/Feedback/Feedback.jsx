7dbf4cb38658137740b38379bd51fd641a5438b7
Oleh Semennykov <sumcue@gmail.com>
2022-05-10 15:25:43 +0300

    add reduce

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,54 +1,95 @@
-import React, { useState } from 'react';
+import { useState, useReducer } from 'react';
 
-// import s from './Feedback.module.css';
+import s from './Feedback.module.css';
 import Statistics from '../Statistics';
 import FeedbackOptions from '../FeedbackOptions';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 import PropTypes from 'prop-types';
-
+function countFeedback(state, action) {
+  switch (action.type) {
+    case 'good':
+      return { ...state, good: state.good + action.payload };
+    case 'bad':
+      return { ...state, bad: state.bad + action.payload };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + action.payload };
+    default:
+      throw new Error('aaaaaaaaaaaaaaaaaaaaaaaaa');
+  }
+}
 export default function Feedback() {
-  const [good, setGood] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-
-  const options = ['good', 'bad', 'neutral'];
-
+  const [state, dispatch] = useReducer(countFeedback, {
+    good: 0,
+    bad: 0,
+    neutral: 0,
+  });
+  // return (
+  //   <div className={s.container}>
+  //     <p>{state.good}</p>
+  //     <p>{state.bad}</p>
+  //     <p>{state.neutral}</p>
+  //     <button
+  //       className={s.button}
+  //       onClick={() => dispatch({ type: 'good', payload: 1 })}
+  //     >
+  //       good
+  //     </button>
+  //     <button
+  //       className={s.button}
+  //       onClick={() => dispatch({ type: 'bad', payload: 1 })}
+  //     >
+  //       bad
+  //     </button>
+  //     <button
+  //       className={s.button}
+  //       onClick={() => dispatch({ type: 'neutral', payload: 1 })}
+  //     >
+  //       neutral
+  //     </button>
+  //   </div>
+  // );
+  // const [good, setGood] = useState(0);
+  // const [bad, setBad] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const options = ['good', 'bad', 'neutral'];
+  // const handleClick = option => {
+  //   // const option = event.target.textContent;
+  //   switch (option) {
+  //     case 'good':
+  //       setGood(prevState => prevState + 1);
+  //       break;
+  //     case 'bad':
+  //       setBad(prevState => prevState + 1);
+  //       break;
+  //     case 'neutral':
+  //       setNeutral(prevState => prevState + 1);
+  //       break;
+  //     default:
+  //       return;
+  //   }
+  // };
+  const keys = Object.keys(state);
   const handleClick = option => {
-    // const option = event.target.textContent;
-
-    switch (option) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-      default:
-        return;
-    }
+    dispatch({ type: option, payload: 1 });
   };
-
   const countTotalFeedback = () => {
-    return good + neutral + bad;
+    return state.good + state.neutral + state.bad;
   };
   const countPositiveFeedbackPercentage = () => {
-    return Math.round((good * 100) / (good + neutral));
+    return Math.round((state.good * 100) / (state.good + state.neutral));
   };
 
   return (
     <div>
       <Section title="Please leave feedback">
-        <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+        <FeedbackOptions options={keys} onLeaveFeedback={handleClick} />
       </Section>
       <Section title="Statistics">
         <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
+          good={state.good}
+          neutral={state.neutral}
+          bad={state.bad}
           total={countTotalFeedback()}
           positivePercentage={countPositiveFeedbackPercentage()}
         ></Statistics>
